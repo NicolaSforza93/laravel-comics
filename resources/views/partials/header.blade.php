b7f5427e7fc039bba168efc35941f05ec3d73a73
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-12-11 17:35:12 +0100

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,42 @@
-header
+<header>
+    <div class="container">
+        <nav class="d-flex align-items-center gap-4 py-3">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+    
+            <ul class="menu d-flex gap-4 mb-0">
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Characters</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Comics</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Movies</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Tv</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Games</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Collectibles</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Videos</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Fans</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">News</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="text-uppercase text-decoration-none text-black">Shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
