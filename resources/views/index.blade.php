7c7bbcb6248bd46f9c9940747e696a0d4823b961
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-12-11 17:04:13 +0100

    added card comics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-pagina home    
+<div class="container">
+    <div class="row row-gap-4">
+        @foreach ($comics as $item)
+            <div class="col-2">
+                <div class="card h-100">
+                    <img src="{{ $item['thumb'] }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h6 class="text-uppercase">{{ $item['series'] }}</h6>
+                    </div>
+                </div>  
+            </div>          
+        @endforeach
+    </div>
+</div>
 @endsection
